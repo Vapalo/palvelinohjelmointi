63b12a40f31cbbe9e863629d3794f87f25a166df
Toni Vapalo <toni.vapalo@gmail.com>
2021-01-20 19:44:41 +0200

    Kaikki 1. viikon tehtävät samassa projektissa

--- ensimmainenViikko/src/main/java/fi/k2021/ensimmainenViikko/web/HelloController.java
+++ ensimmainenViikko/src/main/java/fi/k2021/ensimmainenViikko/web/HelloController.java
@@ -2,14 +2,35 @@ package fi.k2021.ensimmainenViikko.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-	@RequestMapping("hello")
+	@RequestMapping("index")
 	@ResponseBody
-	public String helloGreeting() {
-		return "Hello SpringBoot";
+	public String mainGreeting() {
+		return "This is the main page";
+
 	}
-	
+
+	@Controller
+	public class ContactController {
+		@RequestMapping("contact")
+		@ResponseBody
+		public String contactGreeting() {
+			return "This is the contacts page";
+		}
+	}
+
+	@Controller
+	public class RequestController {
+		@RequestMapping("hello")
+		@ResponseBody
+		public String paramReturn(@RequestParam(name = "location")String location,
+				@RequestParam(name = "name") String name) {
+			return "Welcome to the " + location + " " + name;
+		}
+	}
+
 }
