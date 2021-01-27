6e17efa4a3ef612e7a72f37809dea28f77a74e15
Toni Vapalo <toni.vapalo@gmail.com>
2021-01-27 22:32:00 +0200

    Bookstoren aloitus valmis

---
+++ Bookstore/Bookstore/src/main/java/fi/POkurssi/Bookstore/domain/Book.java
@@ -0,0 +1,69 @@
+package fi.POkurssi.Bookstore.domain;
+
+public class Book {
+
+	private String title, author;
+	private int year, isbn;
+	private Double price;
+
+	public Book(String title, String author, int year, int isbn, Double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
+
+}
