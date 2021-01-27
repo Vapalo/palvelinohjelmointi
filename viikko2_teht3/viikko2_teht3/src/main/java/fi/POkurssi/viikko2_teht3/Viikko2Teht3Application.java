1d748349c7ae1e50e9a6454831856e4d30b02968
Toni Vapalo <toni.vapalo@gmail.com>
2021-01-27 21:41:06 +0200

    Tehtävän aloitus

---
+++ viikko2_teht3/viikko2_teht3/src/main/java/fi/POkurssi/viikko2_teht3/Viikko2Teht3Application.java
@@ -0,0 +1,13 @@
+package fi.POkurssi.viikko2_teht3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Viikko2Teht3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Viikko2Teht3Application.class, args);
+	}
+
+}
---
+++ viikko2_teht3/viikko2_teht3/src/test/java/fi/POkurssi/viikko2_teht3/Viikko2Teht3ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.POkurssi.viikko2_teht3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viikko2Teht3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
