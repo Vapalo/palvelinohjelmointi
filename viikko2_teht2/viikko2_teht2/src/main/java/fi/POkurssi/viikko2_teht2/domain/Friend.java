37711f5ddc83f40ceea362288c04b8e561bae393
Toni Vapalo <toni.vapalo@gmail.com>
2021-01-27 21:40:11 +0200

    Tehtävä valmis

---
+++ viikko2_teht2/viikko2_teht2/src/main/java/fi/POkurssi/viikko2_teht2/domain/Friend.java
@@ -0,0 +1,28 @@
+package fi.POkurssi.viikko2_teht2.domain;
+
+public class Friend {
+	public String name;
+
+	public Friend(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Friend() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+}
+
+
--- viikko2_teht2/viikko2_teht2/src/main/java/fi/POkurssi/viikko2_teht2/web/helloController.java
+++ viikko2_teht2/viikko2_teht2/src/main/java/fi/POkurssi/viikko2_teht2/web/helloController.java
@@ -1,5 +1,31 @@
 package fi.POkurssi.viikko2_teht2.web;
 
-public class helloController {
 
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import fi.POkurssi.viikko2_teht2.domain.Friend;
+
+@Controller
+public class helloController {
+	
+	
+	
+	
+	@GetMapping("/hello")
+		public String helloList(Model model) {
+		
+		ArrayList<Friend> friends = new ArrayList<>();
+		friends.add(new Friend("Tero"));
+		friends.add(new Friend("Seppo"));
+		friends.add(new Friend("Pertti"));
+			
+			model.addAttribute("friends", friends); 
+		
+		return "hello";
+	}
+	
 }
